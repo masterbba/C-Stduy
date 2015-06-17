95a7e0400563ed1c8f2fcc95ee2288fd47eeea37
masterbba <masterbba@gmail.com>
2015-06-18 02:51:41 +0900

    찰스 페졸드 퍼스트 커밋

---
+++ CP_WPF/WPFEmptyProject/EmptyProject/Code.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace SayHello
+{
+    class SayHello
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Window win = new Window();
+            win.Title = "sayHello";
+            win.Show();
+
+            Application app = new Application();
+            app.Run();
+        }
+    }
+}
