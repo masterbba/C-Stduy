01effe609d04497afcb3669ab6c9ec625156f85c
masterbba <masterbba@gmail.com>
2015-06-27 15:15:34 +0900

    Chapter5 StackPanel and WrapPanel -ExploreDirectiories

---
+++ CP_WPF/WPFEmptyProject/EmptyProject/ExploreDirectiories.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ExploreDirectiories
+{
+    class ExploreDirectiories : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new ExploreDirectiories());
+        }
+
+        public ExploreDirectiories()
+        {
+            Title = "Explore Directoires";
+
+            ScrollViewer scroll = new ScrollViewer();
+            Content = scroll;
+            WrapPanel wrap = new WrapPanel();
+            scroll.Content = wrap;
+            wrap.Children.Add(new FileSystemInfoButton());
+
+        }
+    }
+}
