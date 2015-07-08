5040e8e044f3d88f94cc533e0c8331abe5e797aa
masterbba <masterbba@gmail.com>
2015-07-09 02:15:50 +0900

    Chapter8 DependencyProperty - InputEvent

--- CP_WPF/WPFEmptyProject/EmptyProject/SetSpaceProperty.cs
+++ CP_WPF/WPFEmptyProject/EmptyProject/SetSpaceProperty.cs
@@ -1,78 +1,122 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 
-namespace SetSpaceProperty
+namespace ExamineRoutedEvents
 {
-    public class SetSpaceProperty : SpaceWindow
+    public class ExamineRoutedEvents : Application
     {
+        static readonly FontFamily fontfam = new FontFamily("Lucia Consols");
+        const string strForamt = "{0,-30} {1,-15} {2,-15} {3,-15}";
+        StackPanel stackOutput;
+        DateTime dtLast;
+
         [STAThread]
         public static void Main()
         {
-            Application app = new Application();
-            app.Run(new SetSpaceProperty());
+            ExamineRoutedEvents app = new ExamineRoutedEvents();
+            app.Run();
         }
 
-        public SetSpaceProperty()
+        protected override void OnStartup(StartupEventArgs args)
         {
-            Title = "Set Space Property";
-            SizeToContent = SizeToContent.WidthAndHeight;
-            ResizeMode = ResizeMode.CanMinimize;
-            int[] iSpace = { 0, 1, 2 };
+            base.OnStartup(args);
+
+            Window win = new Window();
+            win.Title = "Examine Routed Events";
 
             Grid grid = new Grid();
-            Content = grid;
+            win.Content = grid;
 
-            for (int i = 0; i < 2; i++)
+            RowDefinition rowdef = new RowDefinition();
+            rowdef.Height = GridLength.Auto;
+            grid.RowDefinitions.Add(rowdef);
+
+            rowdef = new RowDefinition();
+            rowdef.Height = GridLength.Auto;
+            grid.RowDefinitions.Add(rowdef);
+
+            rowdef = new RowDefinition();
+            rowdef.Height = new GridLength(100, GridUnitType.Star);
+            grid.RowDefinitions.Add(rowdef);
+
+            Button btn = new Button();
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.Margin = new Thickness(24);
+            btn.Padding = new Thickness(24);
+            grid.Children.Add(btn);
+
+            TextBlock text = new TextBlock();
+            text.FontSize = 24;
+            text.Text = win.Title;
+            btn.Content = text;
+
+            TextBlock textHeadings = new TextBlock();
+            textHeadings.FontFamily = fontfam;
+            textHeadings.Inlines.Add(new Underline(new Run(
+                String.Format(strForamt, "Routed Event", "sender", "Source", "OriginalSoures"))));
+            grid.Children.Add(textHeadings);
+            Grid.SetRow(textHeadings, 1);
+
+            ScrollViewer scroll = new ScrollViewer();
+            grid.Children.Add(scroll);
+            Grid.SetRow(scroll,2);
+
+            stackOutput = new StackPanel();
+            scroll.Content = stackOutput;
+
+            UIElement[] els = { win, grid, btn, text };
+
+            foreach (UIElement el in els)
             {
-                RowDefinition row = new RowDefinition();
-                row.Height = GridLength.Auto;
-                grid.RowDefinitions.Add(row);
+                el.PreviewKeyDown += AllPurposeEventHandler;
+                el.PreviewKeyUp += AllPurposeEventHandler;
+                el.PreviewTextInput += AllPurposeEventHandler;
+                el.KeyDown += AllPurposeEventHandler;
+                el.KeyUp += AllPurposeEventHandler;
+                el.TextInput += AllPurposeEventHandler;
+
+                el.MouseDown += AllPurposeEventHandler;
+                el.MouseUp += AllPurposeEventHandler;
+                el.PreviewMouseDown += AllPurposeEventHandler;
+                el.PreviewMouseUp += AllPurposeEventHandler;
+
+                el.StylusDown += AllPurposeEventHandler;
+                el.StylusUp += AllPurposeEventHandler;
+                el.PreviewStylusDown += AllPurposeEventHandler;
+                el.PreviewStylusUp += AllPurposeEventHandler;
+
+                el.AddHandler(Button.ClickEvent, new RoutedEventHandler(AllPurposeEventHandler));
             }
 
-            for (int i = 0; i < iSpace.Length; i++)
-            {
-                ColumnDefinition col = new ColumnDefinition();
-                col.Width = GridLength.Auto;
-                grid.ColumnDefinitions.Add(col);
-            }
-
-            for (int i = 0; i < iSpace.Length; i++)
-            {
-                SpaceButton btn = new SpaceButton();
-                btn.Text = "Set WIndow Space to " + iSpace[i];
-                btn.Tag = iSpace[i];
-                btn.HorizontalAlignment = HorizontalAlignment.Center;
-                btn.VerticalAlignment = VerticalAlignment.Center;
-                btn.Click += WindowPropertyOnClick;
-                grid.Children.Add(btn);
-                Grid.SetRow(btn, 0);
-                Grid.SetColumn(btn, i);
-
-                btn = new SpaceButton();
-                btn.Text = "Set button Space to " + iSpace[i];
-                btn.Tag = iSpace[i];
-                btn.HorizontalAlignment = HorizontalAlignment.Center;
-                btn.VerticalAlignment = VerticalAlignment.Center;
-                btn.Click += ButtonPropertyOnClcik;
-                grid.Children.Add(btn);
-                Grid.SetRow(btn, 1);
-                Grid.SetColumn(btn, i);
-            }
+            win.Show();
         }
 
-        void WindowPropertyOnClick(object sender, RoutedEventArgs args)
+        void AllPurposeEventHandler(object sender, RoutedEventArgs args)
         {
-            SpaceButton btn = args.Source as SpaceButton;
-            Space = (int)btn.Tag;
+            DateTime dtNow = DateTime.Now;
+            if (dtNow - dtLast > TimeSpan.FromMilliseconds(100))
+                stackOutput.Children.Add(new TextBlock(new Run("")));
+            dtLast = dtNow;
+
+            TextBlock text = new TextBlock();
+            text.FontFamily = fontfam;
+            text.Text = string.Format(strForamt,
+                args.RoutedEvent.Name,
+                TypeWithoutNameSapce(sender),
+                TypeWithoutNameSapce(args.Source),
+                TypeWithoutNameSapce(args.OriginalSource));
+            stackOutput.Children.Add(text);
+            (stackOutput.Parent as ScrollViewer).ScrollToBottom();
         }
 
-        void ButtonPropertyOnClcik(object sender, RoutedEventArgs args)
+        string TypeWithoutNameSapce(object obj)
         {
-            SpaceButton btn = args.Source as SpaceButton;
-            btn.Space = (int)btn.Tag;
+            string[] astr = obj.GetType().ToString().Split('.');
+            return astr[astr.Length - 1];
         }
     }
 }
